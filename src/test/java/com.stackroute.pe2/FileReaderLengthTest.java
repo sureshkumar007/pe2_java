82aa7d13fcd2d2d35e248dcbbedb7c359c4ad693
sureshkumar007 <sureshkumar12074@gmail.com>
2019-07-16 17:33:33 +0530

    File program test created and added

--- src/test/java/com.stackroute.pe2/FileReaderLengthTest.java
+++ src/test/java/com.stackroute.pe2/FileReaderLengthTest.java
@@ -1,5 +1,23 @@
 package com.stackroute.pe2;
 
+import org.junit.Test;
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class FileReaderLengthTest
 {
+    FileReaderLength fileReaderLength=new FileReaderLength();
+    @Test
+    public void function()
+    {
+        String string=fileReaderLength.functionMain("input.txt");
+        assertEquals("HELLO",string);
+    }
+    @Test
+    public void function1()
+    {
+        String string=fileReaderLength.functionMain("abd");
+        assertEquals("false",string);
+    }
+
 }
